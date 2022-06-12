2e6ef9aa150808362b0a0557c96f3d7168a7a308
Ivan Baksheev <zpix-dev@list.ru>
2022-06-13 01:54:10 +0700

    fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import Head from 'next/head';
+import favico from "../public/favicon.ico";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Ivan's personal page</title>
+      <link rel="shortcut icon" href="/about-me/favicon.ico" type="image/x-icon" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
