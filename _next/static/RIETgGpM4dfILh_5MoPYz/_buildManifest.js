f1ab96c588d91f2cf55f906add6020f0fb7b8512
zpix1 <zpix-dev@list.ru>
2022-06-13 05:58:25 +0000

    Deploying to gh-pages from  @ 60eecb31f349b1b9610303aad6cfb3e8ee60afc8 🚀

--- _next/static/RIETgGpM4dfILh_5MoPYz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/547-cd310fdf06a1d81c.js","static/chunks/pages/index-5279505e037a4ff6.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
