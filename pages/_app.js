c67247dd6580d201e111859ba3d1c6f53bd8a1e7
Ivan Baksheev <zpix-dev@list.ru>
2022-06-13 14:23:51 +0700

    meta added

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ function MyApp({ Component, pageProps }) {
   return <>
     <Head>
       <title>Ivan's personal page</title>
+      <meta name="description" content="Ivan's personal page: software engineering, math, computer security. Contacts and CV." />
       <link rel="shortcut icon" href="/about-me/favicon.ico" type="image/x-icon" />
     </Head>
     <Component {...pageProps} />
